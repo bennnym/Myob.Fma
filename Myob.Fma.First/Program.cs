95a22eeb9019b31d6b2a61e350f51d291ec3f6c4
Ben Muller <ben.k.muller@gmail.com>
2019-08-08 10:59:07 +1000

    first commit

---
+++ Myob.Fma.First/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Myob.Fma.First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
