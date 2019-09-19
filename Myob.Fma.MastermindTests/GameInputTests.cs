ccf17386516bb0c16ffc5cd8777da5c4076873fa
Ben Muller <ben.muller@myob.com>
2019-09-20 08:38:24 +1000

    ask question about inline data for mentors

--- Myob.Fma.MastermindTests/GameInputTests.cs
+++ Myob.Fma.MastermindTests/GameInputTests.cs
@@ -10,21 +10,72 @@ namespace Myob.Fma.MastermindTests
 {
     public class GameInputTests
     {
-        [Fact]
-        // [Theory]
-        // [InlineData("red yellow purple green", new List<Colours>(){Colours.RED, Colours.YELLOW, Colours.PURPLE, Colours.GREEN})]
-        public void Should_Return_A_List_Of_Colours_When_Valid_Colours_Are_Entered()
+//        [Fact]
+//        public void Should_Return_A_List_Of_Colours_When_Valid_Colours_Are_Entered()
+//        {
+//            var mockConsoleService = new Mock<ConsoleIoService>();
+//            var inputReader = new GameInput(mockConsoleService.Object);
+//            mockConsoleService.Setup(i => i.GetUserInput()).Returns("red yellow purple green");
+//
+//
+//            // Arrange
+//            var guesses = inputReader.GetUsersCodeGuess();
+//
+//            // Assert
+//            Assert.Equal(new List<Colours>() {Colours.RED, Colours.YELLOW, Colours.PURPLE, Colours.GREEN}, guesses);
+//        }
+        
+//        [Theory]
+//        [InlineData("red yellow purple green", new List<Colours>(){Colours.RED, Colours.YELLOW, Colours.PURPLE, Colours.GREEN})]
+//        public void Should_Return_A_List_Of_Colours_When_Valid_Colours_Are_Entered(string userInput, List<Colours> expectedOutput)
+//        {
+//            var mockConsoleService = new Mock<ConsoleIoService>();
+//            var inputReader = new GameInput(mockConsoleService.Object);
+//            mockConsoleService.Setup(i => i.GetUserInput()).Returns(userInput);
+//
+//
+//            // Arrange
+//            var guesses = inputReader.GetUsersCodeGuess();
+//
+//            // Assert
+//            Assert.Equal(expectedOutput, guesses);
+//        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Should(string userInput, List<Colours> expectedOutput)
         {
+            // Arrange
             var mockConsoleService = new Mock<ConsoleIoService>();
             var inputReader = new GameInput(mockConsoleService.Object);
-            mockConsoleService.Setup(i=> i.GetUserInput()).Returns("red yellow purple green");
-            
+            mockConsoleService.Setup(i => i.GetUserInput()).Returns(userInput);
 
-            // Arrange
+
+            // Act
             var guesses = inputReader.GetUsersCodeGuess();
-            
+
             // Assert
-            Assert.Equal(new List<Colours>(){Colours.RED, Colours.YELLOW, Colours.PURPLE, Colours.GREEN}, guesses);
+            Assert.Equal(expectedOutput, guesses);
         }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    "red yellow purple green",
+                    new List<Colours>() {Colours.RED, Colours.YELLOW, Colours.PURPLE, Colours.GREEN}
+                },
+                new object[]
+                {
+                    "red red red red",
+                    new List<Colours>() {Colours.RED, Colours.RED, Colours.RED, Colours.RED}
+                },
+                new object[]
+                {
+                    "red yellow blue green",
+                    new List<Colours>() {Colours.RED, Colours.YELLOW, Colours.BLUE, Colours.GREEN}
+                },
+            };
     }
 }
