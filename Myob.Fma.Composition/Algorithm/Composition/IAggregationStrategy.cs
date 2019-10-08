91fc41cca42412873acddfa57ac44fd46b0293a5
Ben Muller <ben.muller@myob.com>
2019-10-09 08:17:52 +1100

    deleted folders

--- Myob.Fma.Composition/Algorithm/Composition/IAggregationStrategy.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Algorithm.Composition
-{
-    public interface IAggregationStrategy
-    {
-        Measurement Aggregate(IEnumerable<Measurement> measurements);
-    }
-}
--- Myob.Fma.Composition/Algorithm/Composition/IMeasurementFilter.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Algorithm.Composition
-{
-    public interface IMeasurementFilter
-    {
-        IEnumerable<Measurement> Filter(IEnumerable<Measurement> measurements);
-    }
-}
